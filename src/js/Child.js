a4ae23463410f957e0d6c179e67161581b9ccf0b
Nigel Pegg <nlpegg@gmail.com>
2015-05-16 00:20:13 -0700

    removing unneeded examples

--- src/js/Child.js
+++
@@ -1,9 +0,0 @@
-module.exports = React.createClass({
-    render: function() {
-        return (
-            <span>
-                Hello, world! I am a Child.
-            </span>
-         );
-    }
-});
